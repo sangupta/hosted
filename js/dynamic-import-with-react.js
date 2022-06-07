ef0d4e6206f53d25cd70bd86d657a42c1047fe59
Sandeep Gupta <sangupta@users.noreply.github.com>
2022-06-07 14:54:13 -0700

    Create dynamic-import-with-react.js

---
+++ js/dynamic-import-with-react.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * Replicate the `import` function on window object
+ * so that scripts can use it without conflicting with bundlers.
+ */
+window.__import__ = function(url) {
+  return import(url);
+}
