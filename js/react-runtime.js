49eba8a54a4314f93f5d24650dd9464ea427fd9b
Sandeep Gupta <sangupta@users.noreply.github.com>
2022-09-10 15:09:42 -0700

    Update react-runtime.js

--- js/react-runtime.js
+++ js/react-runtime.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
 export const jsx = React.createElement;
+export const jsxDev = React.createElement;
 export const jsxs = React.createElement;
+export const jsxsDev = React.createElement;
 export const Fragment = React.Fragment;
